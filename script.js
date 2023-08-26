3c43217a94432e92434567e2aeb2050675538261
Vandermeulen Christophe <vandermeulen.christophe@gmail.com>
2023-08-26 17:45:06 +0200

    :sparkles: traduction update & adresse postal update :sparkles:

--- script.js
+++ script.js
@@ -1,39 +1,35 @@
-$(document).ready(function ($) {
-  function animateElements() {
-    $(".progressbar").each(function () {
-      var elementPos = $(this).offset().top;
-      var topOfWindow = $(window).scrollTop();
-      var percent = $(this).find(".circle").attr("data-percent");
-      var animate = $(this).data("animate");
-      if (elementPos < topOfWindow + $(window).height() - 30 && !animate) {
-        $(this).data("animate", true);
-        $(this)
-          .find(".circle")
-          .circleProgress({
-            // startAngle: -Math.PI / 2,
-            value: percent / 100,
-            size: 400,
-            thickness: 15,
-            fill: {
-              color: "#03a9f4",
-            },
-          })
-          .on(
-            "circle-animation-progress",
-            function (event, progress, stepValue) {
-              $(this)
-                .find("strong")
-                .text((stepValue * 100).toFixed(0) + "%");
-            }
-          )
-          .stop();
-      }
-    });
-  }
+// Fonction pour animer les éléments lorsqu'ils sont visibles dans la fenêtre
+// function animateElements() {
+//   document.querySelectorAll(".progressbar").forEach(function (element) {
+//     var elementPos = element.offsetTop;
+//     var topOfWindow = window.scrollY;
+//     var percent = element.querySelector(".circle").getAttribute("data-percent");
+//     var animate = element.dataset.animate;
+//     if (elementPos < topOfWindow + window.innerHeight - 30 && !animate) {
+//       element.dataset.animate = true;
+//       $(element)
+//         .find(".circle")
+//         .circleProgress({
+//           value: percent / 100,
+//           size: 400,
+//           thickness: 15,
+//           fill: {
+//             color: "#03a9f4",
+//           },
+//         })
+//         .on("circle-animation-progress", function (event, progress, stepValue) {
+//           $(this)
+//             .find("strong")
+//             .text((stepValue * 100).toFixed(0) + "%");
+//         })
+//         .stop();
+//     }
+//   });
+// }
 
-  animateElements();
-  $(window).scroll(animateElements);
-});
+// Appeler la fonction animateElements() lors du chargement de la page et lors du défilement
+// animateElements();
+// window.addEventListener("scroll", animateElements);
 
 let progressBars = document.querySelectorAll(".progress");
 progressBars.forEach((bar) => {
@@ -41,7 +37,14 @@ progressBars.forEach((bar) => {
   bar.style.width = progress + "%";
 });
 
-var nbAnnees = document.getElementById("nbAnnees");
-var currentTime = new Date();
-var year = currentTime.getFullYear();
-nbAnnees.innerHTML = year - 2008;
+// Récupérer l'élément HTML par son ID
+const nbAnnees = document.getElementById("nbAnnees");
+// Obtenir la date courante
+const currentTime = new Date();
+const currentYear = currentTime.getFullYear();
+// Année de référence
+const referenceYear = 2008;
+// Calculer le nombre d'années écoulées depuis l'année de référence
+const nbYearsPassed = currentYear - referenceYear;
+// Afficher le résultat dans l'élément HTML
+nbAnnees.innerHTML = nbYearsPassed;
